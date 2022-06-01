9f073e0b76f3f54d730fe70d5325ab28feda061c
George Dainton <gcdainton94@gmail.com>
2022-06-01 14:54:41 +0100

    notesView implemented

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const NotesModel = require("./notesModel");
+const NotesView = require("./notesView");
 
-const notes = new NotesModel;
-console.log(notes.getNotes());
+const notesModel = new NotesModel;
+const notesView = new NotesView(notesModel);
+notesModel.addNote(`Yo Yo Yo it's me again, HOP bad boy`)
+notesView.displayNotes();
 
 console.log('The notes app is running');
